3bd8162afb773500175213d3abd4145628f50211
Guilherme Freire <guilhermefreire841@gmail.com>
2024-11-23 14:45:02 -0300

    Adicionando tratativa para exceções

--- src/app/lib/contentful.js
+++ src/app/lib/contentful.js
@@ -33,77 +33,77 @@ async function requestContentful(url = "") {
 }
 
 export function parseFromMedias(response) {
-  let medias = [];
+  try {
+    let medias = response.items.map(item => {
+      return {
+        id: item.sys.id,
+        title: item.fields.title,
+        description: item.fields.description,
+        thumb: {
+          id: item.fields.thumb.sys.id
+        },
+        content: {
+          id: item.fields.content.sys.id
+        }
+      };
+    });
 
-  if (!response.items) {
+    medias.map(media => {
+      const thumb = response.includes.Asset.find(asset => {
+        return asset.sys.id === media.thumb.id;
+      });
+
+      const content = response.includes.Asset.find(asset => {
+        return asset.sys.id === media.content.id;
+      });
+
+      media.thumb.title = thumb.fields.title;
+      media.thumb.url = `https:${thumb.fields.file.url}`;
+      media.content.title = content.fields.title;
+      media.content.url = `https:${content.fields.file.url}`;
+      return media;
+    });
+
     return medias;
+  } catch (e) {
+    console.error("Ocorreu um erro ao processar as mídias");
+    return [];
   }
-
-  medias = response.items.map(item => {
-    return {
-      id: item.sys.id,
-      title: item.fields.title,
-      description: item.fields.description,
-      thumb: {
-        id: item.fields.thumb.sys.id
-      },
-      content: {
-        id: item.fields.content.sys.id
-      }
-    };
-  });
-
-  medias.map(media => {
-    const thumb = response.includes.Asset.find(asset => {
-      return asset.sys.id === media.thumb.id;
-    });
-
-    const content = response.includes.Asset.find(asset => {
-      return asset.sys.id === media.content.id;
-    });
-
-    media.thumb.title = thumb.fields.title;
-    media.thumb.url = `https:${thumb.fields.file.url}`;
-    media.content.title = content.fields.title;
-    media.content.url = `https:${content.fields.file.url}`;
-    return media;
-  });
-
-  return medias;
 }
 
 export async function parseFromMedia(response) {
+  try {
+    const media = {
+      id: response.sys.id,
+      title: response.fields.title,
+      description: response.fields.description,
+      created_at: response.sys.createdAt,
+      updated_at: response.sys.updatedAt,
+      thumb: {},
+      content: {}
+    };
 
-  if (!response.sys || !response.fields) {
+    const thumb_id = response.fields.thumb.sys.id;
+    const content_id = response.fields.content.sys.id;
+
+    const thumb = await getContent("", ContentTypeEnum.ASSETS, true, thumb_id);
+
+    media.thumb.id = thumb.sys.id;
+    media.thumb.title = thumb.fields.title;
+    media.thumb.description = thumb.fields.description;
+    media.thumb.url = `https:${thumb.fields.file.url}`;
+
+    const content = await getContent("", ContentTypeEnum.ASSETS, true, content_id);
+
+    media.content.id = content.sys.id;
+    media.content.title = content.fields.title;
+    media.content.description = content.fields.description;
+    media.content.url = `https:${content.fields.file.url}`;
+
+    return media;
+
+  } catch (e) {
+    console.error("Ocorreu um erro ao processar a mídia");
     return {};
   }
-
-  const media = {
-    id: response.sys.id,
-    title: response.fields.title,
-    description: response.fields.description,
-    created_at: response.sys.createdAt,
-    updated_at: response.sys.updatedAt,
-    thumb: {},
-    content: {}
-  };
-
-  const thumb_id = response.fields.thumb.sys.id;
-  const content_id = response.fields.content.sys.id;
-
-  const thumb = await getContent("", ContentTypeEnum.ASSETS, true, thumb_id);
-
-  media.thumb.id = thumb.sys.id;
-  media.thumb.title = thumb.fields.title;
-  media.thumb.description = thumb.fields.description;
-  media.thumb.url = `https:${thumb.fields.file.url}`;
-
-  const content = await getContent("", ContentTypeEnum.ASSETS, true, content_id);
-
-  media.content.id = content.sys.id;
-  media.content.title = content.fields.title;
-  media.content.description = content.fields.description;
-  media.content.url = `https:${content.fields.file.url}`;
-
-  return media;
 }
